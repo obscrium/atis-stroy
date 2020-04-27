608939b2d84ecf2c66cb7ed2178418fd9c26c1d3
obscrium <sparkygino@gmail.com>
2020-04-27 19:58:23 +0300

    Third commit

--- src/Components/CarouselBox.js
+++ src/Components/CarouselBox.js
@@ -7,7 +7,7 @@ export default class CarouselBox extends Component {
     render() {
         return (
             <Container>
-            <Carousel >
+            <Carousel>
                 <Carousel.Item >
                     <img
                         className="d-block w-100 item"
